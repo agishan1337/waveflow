6bf94828fa23d604fa0ee6ffe4876b276e721e42
Ishan <ishan@perimeters.io>
2025-01-21 13:17:05 +0530

    ProductShowcase Base

--- components/HeroSection/HeroSection.vue
+++ components/HeroSection/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
-    <section class="relative pt-40 pb-24 flex flex-col gap-16 items-center justify-center px-4 md:px-16 lg:px-24 overflow-hidden">
-        <div class="flex flex-col gap-8 lg:max-w-4xl z-10">
+    <section class="relative pt-40 pb-12 md:pb-16 lg:pb-24 flex flex-col gap-8 md:gap-12 lg:gap-16 items-center justify-center px-4 md:px-16 lg:px-24 ">
+        <div class="flex flex-col gap-4 md:gap-8 lg:max-w-4xl z-10">
             <Tag label="We have just launched!" />
             <h1 class="text-h3 md:text-h2 md:font-semibold lg:text-h1 lg:font-semibold font-Poppins text-center font-semibold "> The <span class="text-primary"> AI notepad </span>people in
                 back-to-back meetings</h1>
@@ -9,7 +9,7 @@
         <div class="z-10 ">
             <img src="assets/HeroAssets/HeroImage.png" alt="">
         </div>
-        <div class="absolute inset-0  w-full h-full overflow-hidden z-0 ">
+        <div class="absolute inset-0  w-full h-full z-0 ">
             <img class="w-full opacity-60" src="assets/HeroAssets/Background.png" alt="">
     </div>
     </section>
