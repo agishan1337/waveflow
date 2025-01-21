9a37b10b107ec18c0772bbb511753fd6ac4d8099
Ishan <ishan@perimeters.io>
2025-01-21 14:50:26 +0530

    Changes

--- components/UseCases/Usecases.vue
+++ components/UseCases/Usecases.vue
@@ -6,7 +6,7 @@ import UsecasesCard from './UsecasesCard.vue';
 <template >
     <section id="usecases" class="relative py-12 md:py-16 lg:py-32 flex flex-col gap-8 md:gap-12 lg:gap-16 items-center justify-center px-4 md:px-16 lg:px-24 bg-bgDark">
           <div class="flex flex-col gap-4 md:gap-8 lg:max-w-4xl z-10 justify-center items-center">
-            <h2 class="text-h2 font-Poppins font-medium text-center text-white">Built for every knowledge worker across your team</h2> 
+            <h2 class="text-body-extra-large md:text-h3 lg:text-h2 font-Poppins font-medium text-center text-white">Built for every knowledge worker across your team</h2> 
             <Button label="Get Started for Free" variant="secondary"/>  
          </div>
          <div class="lg:max-w-7xl z-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6">
