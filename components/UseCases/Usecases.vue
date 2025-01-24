5093f888b29f5104c652db3dbf8e0dd528b16f21
Ishan <ishan@perimeters.io>
2025-01-24 11:27:00 +0530

    Usecases light mode

--- components/UseCases/Usecases.vue
+++ components/UseCases/Usecases.vue
@@ -15,10 +15,10 @@ defineProps<{
 </script>
 
 <template >
-    <section id="usecases" class="relative py-12 md:py-16 lg:py-32 flex flex-col gap-8 md:gap-12 lg:gap-16 items-center justify-center px-4 md:px-16 lg:px-24 bg-bgDark">
+    <section class="relative py-12 md:py-16 lg:py-32 flex flex-col gap-8 md:gap-12 lg:gap-16 items-center justify-center px-4 md:px-16 lg:px-24 ">
           <div class="flex flex-col gap-4 md:gap-8 lg:max-w-4xl z-10 justify-center items-center">
-            <h2 class="text-body-extra-large md:text-h3 lg:text-h2 font-Sora font-medium text-center text-white">{{ title }}</h2> 
-            <Button :label="buttonLabel" variant="secondary"/>  
+            <h2 class="text-body-extra-large md:text-h3 lg:text-h2 font-Sora font-semibold lg:font-semibold text-center text-textPrimary">{{ title }}</h2> 
+            <Button :label="buttonLabel" variant="primary"/>  
          </div>
          <div class="lg:max-w-7xl z-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6">
             <UsecasesCard 
@@ -29,8 +29,5 @@ defineProps<{
               :description="useCase.description"
             />
          </div>
-          <div class="absolute inset-x-0 bottom-0 w-full h-full z-0">
-            <img class="absolute bottom-0 w-full h-1/2 object-bottom" src="/assets/UseCases/Background.png" alt="">
-          </div>
     </section>
 </template>
--- components/UseCases/UsecasesCard.vue
+++ components/UseCases/UsecasesCard.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="rounded-xl flex flex-col gap-4 p-6 border border-darkStrokeColour bg-bgDark bg-opacity-70 backdrop-blur-lg hover:bg-darkHover transition-all duration-300">
+    <div class="rounded-xl flex flex-col gap-4 p-6 border border-strokeColor hover:bg-greyHover transition-all duration-300">
         <img class="w-8 h-8 " :src="icon" alt="">
-        <h3 class="text-body-large text-white text-regular font-Sora font-medium">{{ title }}</h3>
-        <p class="text-small text-white text-regular text-opacity-75 font-Sora">{{ description }}</p>
+        <h3 class="text-body-large text-textPrimary text-regular font-Sora font-medium">{{ title }}</h3>
+        <p class="text-small text-textSecondary text-regular text-opacity-75 font-Sora">{{ description }}</p>
     </div>
 </template>
 
